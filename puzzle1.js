02d01a4c230456ffcebdd5d3dc93a0abe060fc53
danqian <danqian@cca.edu>
2017-01-21 22:30:50 -0800

    Create puzzle1.js

---
+++ puzzle1.js
@@ -0,0 +1,15 @@
+// Help Rudy the Red Dot Collect all the green dots!
+
+// Control Rudy with the following commands:
+//  up();                   --moves Rudy up one square
+//  down();                 --moves Rudy down one square
+//  left();                 --moves Rudy left one square
+//  right();                --moves Rudy right one square
+
+//Interact with Rudy's environment using the following commands:
+//  remainingDots();        --returns the number of green dots remaining
+//  getColor();             --returns the color of Rudy's square
+//  setColor(color);        --changes the color of Rudy's square, open gates if colors match
+
+down();
+right();
